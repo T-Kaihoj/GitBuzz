b2041dab7ee400bc308021f5cd54ae5b46c4457a
skaihoj <skaihoj@yahoo.dk>
2018-09-14 16:40:10 +0200

    fixed stuff nanoq broke

--- Team1.py
+++ Team1.py
@@ -2,14 +2,13 @@ for x in range(0, 100):
     if x % 11 == 0:
         print('PULL');
     else:
-        print(x);
-
-    if (x % 10 == 0):
-        print("github")
-
-    else:
-        if (x % 9 == 0):
-            print("Version")
+        if (x % 10 == 0):
+            print("github")
         else:
-            if (x % 3 == 0):
-                print("fizz")
+            if (x % 9 == 0):
+                print("Version")
+            else:
+                if (x % 3 == 0):
+                    print("fizz")
+                else:
+                    print(x);
