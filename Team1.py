b3e57878325bac764d265b959ba69bec062eea96
ZzimonNanoq <scalli15@student.aau.dk>
2018-09-14 16:32:56 +0200

    fixattempt

--- Team1.py
+++ Team1.py
@@ -1,11 +1,12 @@
 for x in range(0, 100):
     if x % 11 == 0:
         print('PULL');
+    else:
+        print(x);
+
     if (x % 10 == 0):
         print("github")
 
     else:
         if (x % 9 == 0):
             print("Version")
-        else:
-            print(x);
