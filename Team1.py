ee2a387f0b931ca48d2a1f98d51c4c58234399b4
MartinJensen37 <martin.jensen37@gmail.com>
2018-09-14 16:50:33 +0200

    More stuff added

--- Team1.py
+++ Team1.py
@@ -1,4 +1,6 @@
 for x in range(0, 100):
+    if(x%5==0 and x%3==0):
+        print('FizzBuzz')
 
     if(x%5==0 and x%3==0 and x%2 ==0):
 
@@ -6,7 +8,7 @@ for x in range(0, 100):
 
     if x % 11 == 0:
         print('PULL')
-        
+
     else:
         if (x % 10 == 0):
             print("github")
